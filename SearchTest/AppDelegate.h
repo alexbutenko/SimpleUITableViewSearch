6e8fead62433740c0e41f2145fb44cfc64e4a36c
alexbutenko <alexandr.butenko@gmail.com>
2014-07-22 12:30:58 +1200

    Initial Commit

---
+++ SearchTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SearchTest
+//
+//  Created by alexbutenko on 7/22/14.
+//  Copyright (c) 2014 alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SearchTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SearchTest
+//
+//  Created by alexbutenko on 7/22/14.
+//  Copyright (c) 2014 alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
